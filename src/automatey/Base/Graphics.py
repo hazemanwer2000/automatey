eef94e14345c16fb66604430294e7cb5f7e72792
hazemanwer2000 <hazemanwer2000@proton.me>
2024-12-05 22:00:05 +0200

    Graphics!

--- src/automatey/Base/Graphics.py
+++ src/automatey/Base/Graphics.py
@@ -69,5 +69,54 @@ class Color:
                      compuMethod(RGBFormat[1]),
                      compuMethod(RGBFormat[2]))
 
-BLACK = Color(0, 0, 0)
-WHITE = Color(255, 255, 255)
+class Colors:
+    
+    BLACK = Color(0, 0, 0)
+    WHITE = Color(255, 255, 255)
+    TRANSPARENT = None
+
+class Point:
+    '''
+    Representation of a point.
+    '''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    
+    def __add__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+    
+class Border:
+    '''
+    Representation of a border.
+    '''
+    def __init__(self, thickness, color:Color):
+        self.thickness = thickness
+        self.color = color
+        
+class Shape:
+    '''
+    Representation of a shape.
+    
+    If the fill-color is 'None', the shape is internally transparent.
+    '''
+
+    def __init__(self, fillColor:Color=Colors.TRANSPARENT, border:Border=None):
+        self.fillColor = fillColor
+        self.border = border
+        
+        # For ease of processing.
+        if (self.border == None):
+            self.border = Border(0, self.fillColor)
+
+class Rectangle(Shape):
+    '''
+    Representation of a rectangle.
+    '''
+    def __init__(self, topLeft:Point, bottomRight:Point, fillColor:Color=Colors.TRANSPARENT, border:Border=None):
+        Shape.__init__(self, fillColor=fillColor, border=border)
+        self.topLeft = topLeft
+        self.bottomRight = bottomRight
