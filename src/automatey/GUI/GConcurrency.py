772dacd4190ceb37dd75ce562f5ed49619c9b89c
hazemanwer2000 <hazemanwer2000@proton.me>
2024-12-13 16:17:46 +0200

    Thread-Safe Queue!

--- src/automatey/GUI/GConcurrency.py
+++ src/automatey/GUI/GConcurrency.py
@@ -35,3 +35,44 @@ class GThread(QtCore.QThread):
     @QtCore.pyqtSlot()
     def run(self):
         self.mainFcn(self)
+
+class GQueue:
+    '''
+    A thread-safe queue.
+    '''
+    
+    def __init__(self):
+        self.queue = []
+        self.mutex = QtCore.QMutex()
+        self.condition = QtCore.QWaitCondition()
+    
+    def GEnqueue(self, data):
+        '''
+        Enqueue data.
+        '''
+        self.mutex.lock()
+        self.queue.append(data)
+        self.condition.wakeOne()
+        self.mutex.unlock()
+
+    def GDequeue(self):
+        '''
+        Dequeue data.
+        '''
+        self.mutex.lock()
+        while len(self.queue) == 0:
+            self.condition.wait(self.mutex)
+        data = self.queue.pop(0)
+        self.mutex.unlock()
+        return data
+    
+    def GTryDequeue(self):
+        '''
+        Dequeue data, if data is available.
+        '''
+        self.mutex.lock()
+        data = None
+        if len(self.queue) > 0:
+            data = self.queue.pop(0)
+        self.mutex.unlock()
+        return data
