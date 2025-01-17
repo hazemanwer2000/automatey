5339c43a239185adc6acf3c71ea6333fc5ba869e
hazemanwer2000 <hazemanwer2000@proton.me>
2025-01-17 13:43:09 +0200

    CLI: LineOverwriter

--- src/automatey/Utils/CLI.py
+++ src/automatey/Utils/CLI.py
@@ -37,6 +37,9 @@ def echo(message:str, textColor:Graphics.TextColor=None):
 class ProgressBar:
     '''
     Progress-bar renderer, that calls `fcn` with `i` in range `0` to `N-1`, inclusive.
+    
+    Note that,
+    - A new-line is echo'ed at the end.
     '''
     def __init__(self, fcn, N:int, label:str):
         self.fcn = fcn
@@ -50,3 +53,44 @@ class ProgressBar:
         with click.progressbar(range(self.iterations), label=self.label) as bar:
             for i in bar:
                 self.fcn(i)
+
+class LineOverwriter:
+    '''
+    Overwrites previous message with every call, until is skipped to next line.
+    
+    Note that,
+    - To work properly, message may not contain a new-line character.
+    '''
+    class INTERNAL:
+        
+        TrackedMaxMessageLength = 0
+        IsCurrentlyTracking = False
+    
+    @staticmethod
+    def write(message:str, textColor:Graphics.TextColor=None):
+        
+        # ? (...)
+        currentMessageLength = len(message)
+        
+        # ? Contruct prefix and suffix.
+        prefix = ''
+        suffix = ''
+        if LineOverwriter.INTERNAL.IsCurrentlyTracking:
+            prefix = ('\b' * LineOverwriter.INTERNAL.TrackedMaxMessageLength)
+            CurrentDeltaLength = (LineOverwriter.INTERNAL.TrackedMaxMessageLength - currentMessageLength)
+            if CurrentDeltaLength > 0:
+                suffix = (' ' * CurrentDeltaLength)
+            
+        # ? Update tracked maximum message length.
+        LineOverwriter.INTERNAL.IsCurrentlyTracking = True
+        LineOverwriter.INTERNAL.TrackedMaxMessageLength = max(LineOverwriter.INTERNAL.TrackedMaxMessageLength, currentMessageLength)
+
+        # ? Echo (all).
+        echo(prefix + message, textColor=textColor)
+        echo(suffix)
+    
+    @staticmethod
+    def skipToNextLine():
+        LineOverwriter.INTERNAL.TrackedMaxMessageLength = 0
+        LineOverwriter.INTERNAL.IsCurrentlyTracking = False
+        echo('\n')
