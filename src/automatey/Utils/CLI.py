52dd1e89cb14802281d5752f03f0a0f947603430
hazemanwer2000 <hazemanwer2000@proton.me>
2025-01-24 21:08:46 +0200

    CLI: ProgressBar

--- src/automatey/Utils/CLI.py
+++ src/automatey/Utils/CLI.py
@@ -42,23 +42,22 @@ def echo(message:str, textColor:Graphics.TextColor=None):
 
 class ProgressBar:
     '''
-    Progress-bar renderer, that calls `fcn` with `i` in range `0` to `N-1`, inclusive.
+    Progress-bar renderer, that provides an iterator, with `i` in range `0` to `N-1`, inclusive.
     
     Note that,
     - A new-line is echo'ed at the end.
+    - `close` must called at the end.
     '''
-    def __init__(self, fcn, N:int, label:str):
-        self.fcn = fcn
-        self.iterations = N
+    def __init__(self, N:int, label:str):
         self.label = label
-        
-    def render(self):
-        '''
-        Renders the progress-bar.
-        '''
-        with click.progressbar(range(self.iterations), label=self.label) as bar:
-            for i in bar:
-                self.fcn(i)
+        self.progressbar = click.progressbar(range(N), label=self.label)
+        self.iterator = self.progressbar.__enter__()
+    
+    def getIterator(self):
+        return self.iterator
+    
+    def close(self):
+        self.progressbar.__exit__(None, None, None)
 
 class LineOverwriter:
     '''
